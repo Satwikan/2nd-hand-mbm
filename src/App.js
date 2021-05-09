cdaeed02b2af018a3ffb6414f9bfe236b0dde512
Rajat <${GITHUB_ACTOR}@users.noreply.github.com>
2021-05-09 16:23:21 +0530

    Seller Page [fileName -- Sell.js]

--- src/App.js
+++ src/App.js
@@ -6,60 +6,88 @@ import "mdbreact/dist/css/mdb.css";
 
 import Header from "./Header"; //Include Heder
 import Footer from "./Footer"; //Include Footer
+import Sell from "./Sell";
 
 import { Row, Col, Container } from "react-bootstrap";
 import Card from "./Card";
 import Banner from "./Banner";
 
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Header />
+/* <Container className="w-65 p-3">
+
+<div className="App">
+ <br/>
+ 
+ <Sell/>
+</div>
+
+</Container> */
+
+
+
+
+
+       <div className="App">
+        
+          
+         <Header />
         <br />
         <br />
         <Banner />
         <br />
         <br />
         <br />
+        <Container>
+          <Row>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+          </Row>
+        </Container>
+        <br />
+        <br />
+        <Container>
+          <Row>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+            <Col>
+              <Card />
+            </Col>
+          </Row>
+        </Container>
+     <Container className="w-65 p-3">
 
-        <Container>
-          <Row>
-            <Col>
-              <Card />
-            </Col>
-            <Col>
-              <Card />
-            </Col>
-            <Col>
-              <Card />
-            </Col>
-          </Row>
-        </Container>
+<div className="App">
+ <br/>
+ 
+ <Sell/>
+</div>
+
+</Container>
         <br />
-        <br />
-        <Container>
-          <Row>
-            <Col>
-              <Card />
-            </Col>
-            <Col>
-              <Card />
-            </Col>
-            <Col>
-              <Card />
-            </Col>
-          </Row>
-        </Container>
-        <br />
-        <br />
-    
-        <Footer />
+        <br />    
+        <Footer /> 
+      
+   
+
   
-    
+   
 
 
-      </div>
+      </div> 
     );
   }
 }
