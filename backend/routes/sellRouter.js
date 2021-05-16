04d6ea4373e79ffda586868cfae4da7799036c0b
Satwikan <satwikanmol25@gmail.com>
2021-05-16 15:49:49 +0530

    added mongodb

--- backend/routes/sellRouter.js
+++ backend/routes/sellRouter.js
@@ -1,13 +1,21 @@
-var express = require("express");
-var sell = express.Router();
+const express = require("express");
+const sell = express.Router();
+const Ads = require("../models/Ads");
 
 sell.use(express.json());
-/* GET users listing. */
-sell.post("/", (req, res, next) => {
-  console.log(req.body)
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json(req.body);
+
+sell.post("/", async (req, res, next) => {
+  try {
+    console.log("Received Body:\n", req.body);
+    const ad = await Ads.create(req.body);
+    console.log("Ad Created:", ad);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json(ad);
+  } catch (err) {
+    next(err);
+    console.log("Error:\n", err);
+  }
 });
 
 module.exports = sell;
