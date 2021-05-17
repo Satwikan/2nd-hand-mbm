efe955f3158b4bddbb6a2945665c91f23af5471f
Rajat <${GITHUB_ACTOR}@users.noreply.github.com>
2021-05-17 10:36:19 +0530

    Again Background

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -14,7 +14,7 @@ import history from "../history";
 class Header extends React.Component {
   render() {
     return (
-      <Navbar bg="lg" expand="lg">
+      <Navbar bg="danger" expand="lg">
         <Navbar.Brand onClick={() => history.push('/')}> <strong> 2nd HAND-MBM </strong> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
